3be376a9f12847b13b8a06b24ba1232ebae147a4
SenadSC <81653812+SenadSC@users.noreply.github.com>
2021-07-01 14:47:37 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+//Del 1//
+
+var x = 5;
+const y = 5;
+
+//Del 2//
+
+1;
+var x = myFunction(4, 3);
+function myFunction(a, b) {
+  return a * b;
+}
+
+2;
+
+3;
+function myFunction(a, b) {
+  return a + b;
+}
+
+4;
+let a = 4;
+let b = 2;
+
+(a, b) => {
+  let John = 42;
+  return a + b + John;
+};
+
+5;
+function checkIt() {
+  var agree = confirm("Vill du?");
+
+  if (agree) {
+    return true;
+  } else {
+    return false;
+  }
+}
